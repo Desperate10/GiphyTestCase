285e150748780012165ef9b90ad860c5fec36e8b
Yevhenii Hromov <gromov.zhenya@gmail.com>
2023-01-16 11:35:03 +0200

    - Created Database DI module - Added constant field for db name

--- app/src/main/java/desperate/giphytestcase/utils/Constants.kt
+++ app/src/main/java/desperate/giphytestcase/utils/Constants.kt
@@ -5,5 +5,6 @@ object Constants {
     const val BASE_URL = "https://api.giphy.com/v1/gifs/"
     const val GIPHY_IMAGE_TABLE = "giphy_table"
     const val GIPHY_REMOTE_KEYS = "giphy_remote_keys"
+    const val GIPHY_DATABASE = "giphy_database"
 
 }
