55935aa8e729b6f0e65497adb32e7ffa3cf22b18
Yevhenii Hromov <gromov.zhenya@gmail.com>
2023-01-16 09:44:38 +0200

    Created repository interface

---
+++ app/src/main/java/desperate/giphytestcase/domain/repository/GiphyRepository.kt
@@ -0,0 +1,13 @@
+package desperate.giphytestcase.domain.repository
+
+import androidx.paging.PagingData
+import desperate.giphytestcase.domain.model.Gif
+import kotlinx.coroutines.flow.Flow
+
+interface GiphyRepository {
+
+    fun getTrending(): Flow<PagingData<Gif>>
+
+    fun search(query: String): Flow<PagingData<Gif>>
+
+}
