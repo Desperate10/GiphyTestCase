1d4de99fbceb75a263f31930c875686142f2c563
Yevhenii Hromov <gromov.zhenya@gmail.com>
2023-01-18 16:15:21 +0200

    - Edit NetworkModule.kt using Gson to serialization

--- app/src/main/java/desperate/giphytestcase/di/NetworkModule.kt
+++ app/src/main/java/desperate/giphytestcase/di/NetworkModule.kt
@@ -1,18 +1,15 @@
 package desperate.giphytestcase.di
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import desperate.giphytestcase.data.remote.GiphyApi
 import desperate.giphytestcase.utils.Constants.BASE_URL
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType
-import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -38,15 +35,10 @@ object NetworkModule {
     @Provides
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient) : Retrofit {
-        val contentType = "application/json".toMediaType()
-        val json = Json {
-            ignoreUnknownKeys = true
-        }
-
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(okHttpClient)
-            .addConverterFactory(json.asConverterFactory(contentType))
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
